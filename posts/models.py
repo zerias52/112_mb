411a7e8ed7c4df710a1732b33aa29a356620f5f0
zerias52 <bbyrd52@gmail.com>
2024-10-22 21:03:13 -0600

    Various updates to posts

--- posts/models.py
+++ posts/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):                                           #inheritance
     subtitle = models.CharField(max_length=256)                     #composition
     body = models.TextField()                                       #composition
     created_on = models.DateTimeField(auto_now_add=True)            #composition
+
+    def __str__(self):
+        return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import (
+    CreateView,
+    ListView,
+    DetailView,
+)
+from .models import Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    template_name = 'posts/list.html'
+    model = Post
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+
+class PostCreateView(CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ["title", "subtitle", "body"]
